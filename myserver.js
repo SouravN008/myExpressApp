ca86270c217c4533df0b528cf23893adcc81061a
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-09-05 22:40:11 +0530

    learnt about 'req.params' and 'req.query'

--- myserver.js
+++ myserver.js
@@ -1,14 +1,9 @@
  const express = require ('express');
  const app = express();
  const port = 3000;
- app.get('/users', (req, res) => {
-   res.send("Hello Users!");
- })
- app.get('/admin', (req, res) => {
-  res.send("Admin Page!");
-})
+ app.use(express.static("public"));
 app.get('/', (req, res) => {
-  res.send("Wellcome to my server!");
+  console.log(req.query);
 })
  app.listen(port, ()=>{
      console.log('My server app is listening on the port${300}! http://localhost:3000')  
