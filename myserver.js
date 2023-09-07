5ed1b49fda4e198383fd3144a3c79156b1db047c
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-09-07 21:00:32 +0530

    routers functions in one file

--- myserver.js
+++ myserver.js
@@ -1,12 +1,23 @@
  const express = require ('express');
  const app = express();
  const port = 3000;
- app.set('view engine' ,'ejs');
+ 
 
  app.get('/', (req, res) => {
-  res.render('newResponse');
+  res.send("Homepage");
 });
-
+app.get('/users', (req, res) => {
+  res.send('Users page');
+});
+app.get('/users/new', (req, res) => {
+  res.send('New users page');
+});
+app.get('/course', (req, res) => {
+  res.send('course listing');
+});
+app.get('/course/FSD', (req, res) => {
+  res.send('Full Stack Development');
+});
 
  app.listen(port, ()=>{
      console.log('My server app is listening on the port ${port}! http://localhost:3000')  
