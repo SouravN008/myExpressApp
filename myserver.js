1e6769dfae18518bba53b948cc0c3a855493ab01
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-09-07 13:38:42 +0530

    res.send res.render and res.sendFile

--- myserver.js
+++ myserver.js
@@ -1,21 +1,13 @@
  const express = require ('express');
  const app = express();
  const port = 3000;
- const bodyParser = require('body-parser')
- app.use(bodyParser.urlencoded({extended:false}));
- app.use(express.static("public"));
+ app.set('view engine' ,'ejs');
 
  app.get('/', (req, res) => {
-  res.send("Go to the emails path!");
-})
-app.get('/emails', (req, res) => {
-  res.sendFile(__dirname+ "/public/index.html");
-})
-app.post('/emails' ,(req , res) =>{
-  const email = req.body.email;
-  console.log(email);
-  res.send("Email logged successfully!");
-})
+  res.render('newResponse');
+});
+
+
  app.listen(port, ()=>{
-     console.log('My server app is listening on the port${300}! http://localhost:3000')  
+     console.log('My server app is listening on the port ${port}! http://localhost:3000')  
  })
