a0653dc7c8e92c0e9c2b0d25343b6bce2aa78640
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-09-07 12:25:44 +0530

    post methods

--- myserver.js
+++ myserver.js
@@ -1,9 +1,20 @@
  const express = require ('express');
  const app = express();
  const port = 3000;
+ const bodyParser = require('body-parser')
+ app.use(bodyParser.urlencoded({extended:false}));
  app.use(express.static("public"));
-app.get('/', (req, res) => {
-  console.log(req.query);
+
+ app.get('/', (req, res) => {
+  res.send("Go to the emails path!");
+})
+app.get('/emails', (req, res) => {
+  res.sendFile(__dirname+ "/public/index.html");
+})
+app.post('/emails' ,(req , res) =>{
+  const email = req.body.email;
+  console.log(email);
+  res.send("Email logged successfully!");
 })
  app.listen(port, ()=>{
      console.log('My server app is listening on the port${300}! http://localhost:3000')  
